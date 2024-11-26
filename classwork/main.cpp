4b4035442a56e0216374713add2d776a38dab42e
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-26 17:26:50 -0300

    Update main.cpp

--- classwork/main.cpp
+++ classwork/main.cpp
@@ -18,9 +18,46 @@ enum Controle{LIGA_LIMPA_ERRO, DESLIGA, MEMORIA_LEITURA_ESCRITA, MEMORIA_SOMA, M
 class Tela { // Display de 7 segmentos e 8 digitos + multiplexação + GND + M/E = +-26 fios
     
     public:
-        void adicione(Digito digito){};
-        void adicione(Operacao operacao){};
-        void adicione(Controle controle){};
+        void adicione(Digito digito){
+            switch (digito){
+            case ZERO:
+                std::cout << "0 ";
+                break;
+            case UM:
+                std::cout << "1 ";
+                break;
+            case DOIS:
+                std::cout << "2 ";
+                break;
+            case TRES:
+                std::cout << "3 ";
+                break;
+            case QUATRO:
+                std::cout << "4 ";
+                break;
+            case CINCO:
+                std::cout << "5 ";
+                break;
+            case SEIS:
+                std::cout << "6 ";
+                break;
+            case SETE:
+                std::cout << "7 ";
+                break;
+            case OITO:
+                std::cout << "8 ";
+                break;
+            case NOVE:
+                std::cout << "9 ";
+                break;
+            default:
+            std::cout << "Digito ainda não implementado";
+                break;
+            }
+        };
+        void limpe(){
+            std::cout << "\n\n\n\n\n";
+        };
 };
 
 class Cpu {
@@ -29,10 +66,10 @@ class Cpu {
 
 class Teclado {
     Cpu* cpu;
-    std::vector<Tecla&> teclas;
+    std::vector<Tecla*> teclas;
 
     public: void adicioneTecla(Tecla& tecla){
-        teclas.push_back(tecla);
+        teclas.push_back(&tecla);
     }
 };
 
@@ -55,6 +92,11 @@ int main(){
 
     Teclado teclado1;
     teclado1.adicioneTecla(tecla1);
+
+    Tela t1;
+    t1.adicione(ZERO);
+    t1.adicione(UM);
+    t1.limpe();
 
     std::cout << "All it's okay!";
     return 0;
