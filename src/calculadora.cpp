ac50ae39305ab70464e9d25e890f97709c707ee4
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-12-03 17:14:00 -0300

    Update calculadora.cpp

--- src/calculadora.cpp
+++ src/calculadora.cpp
@@ -55,20 +55,3 @@ void Teclado::adicioneTecla(Tecla &tecla)
 
 void Tecla::pressione() {
 };
-
-int main()
-{
-    Tecla tecla1;
-    tecla1.pressione();
-
-    Teclado teclado1;
-    teclado1.adicioneTecla(tecla1);
-
-    Tela t1;
-    t1.adicione(ZERO);
-    t1.adicione(UM);
-    t1.limpe();
-
-    std::cout << "All it's okay!";
-    return 0;
-};
